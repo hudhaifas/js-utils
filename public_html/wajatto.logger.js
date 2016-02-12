080cd55b59a8a9189f48f2fbb8ede65fa334eb0a
Hudhaifa Shatnawi <hudhaifa.shatnawi@gmail.com>
2016-02-11 18:43:34 -0600

    This JS util helps managing/customizing the print out mechanism og the log messages, the main reason of developing this util is to enable debugging Javascrip on mobile devices - when you don't have Javascript Developer Tool in the handset, this util will print out the log messages on the screen.

---
+++ public_html/wajatto.logger.js
@@ -0,0 +1,54 @@
+/**
+ * @author Hudhaifa Shatnawi <hudhaifa.shatnawi@gmail.com>
+ * @version 1.0 Apr 24, 2015 - 01:41:35 PM
+ */
+
+/**
+ * This JS util helps managing/customizing the print out mechanism og the log messages, the main reason of 
+ * developing this util is to enable debugging Javascrip on mobile devices - when you don't have Javascript 
+ * Developer Tool in the handset, this util will print out the log messages on the screen.
+ */
+var Logger = Logger || {};
+
+(function () {
+    /**
+     * Logger can be:
+     * - ture; to use the system console.log()
+     * - false; to stop showing log messages
+     * - 'OnScreen'; to show the log messages on the screen, used to debug on mobile phones
+     */
+    Logger.LOGGER = false;
+
+    /**
+     * Prints the debug message on screen for mobile devices
+     * 
+     * @param {type} msg The log message
+     */
+    Logger.debug = function (msg) {
+        if (Logger.LOGGER === true) {
+            console.log(msg);
+
+        } else if (Logger.LOGGER === 'OnScreen') {
+            $debugger = $('body').find('#debug-onscreen');
+            if (!$debugger.length) {
+                $('body').append('<div id="debug-onscreen" class="debug-onscreen"></div>');
+                $debugger = $('body').find('#debug-onscreen');
+            }
+
+            $debugger.append(msg + '<br />');
+        }
+    };
+
+    /**
+     * Changes the default error print to use the custom log method.
+     * 
+     * @see debug()
+     */
+    Logger.initJSDebug = function () {
+        Logger.debug('JS Errors for page: ' + window.location.href);
+
+        window.onerror = function (error, url, line) {
+            Logger.debug(error + '\t || \t' + url + ':' + line);
+        };
+    };
+})();
